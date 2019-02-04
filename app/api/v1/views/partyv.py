cea0a35c290bc5a4b6002c0f33c0e7b74df325ad
Muigai <oscarmg.university@gmail.com>
2019-02-05 00:52:44 +0300

    [finished #163682101] getting list of all parties

---
+++ app/api/v1/views/partyv.py
@@ -0,0 +1,22 @@
+from flask import Flask,request, jsonify, make_response
+from app.api.v1.models.Party import PartyClass as party
+
+#list of parties 
+parties_list=[]
+party1=party(1,"chama cha mtetezi","334 kwetu","images/govn.jpg")
+party2=party(1,"chama cha wamama","Makwao street","images/govn.jpg")
+parties_list.append(party1)
+parties_list.append(party2)
+
+class parties():
+    def get(self,id=None):
+        
+        partyData=[]
+        #loop through party data dictionary
+        for party in parties_list:
+            partyData.append({'status':200,'data':{'id':party.id,'name':party.name,'hqAddress':party.hqAddress,'logoUrl':party.logoUrl}})
+
+        #return data in json form
+        return jsonify(partyData)
+
+    
