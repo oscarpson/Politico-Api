c31b018e3be1b508057df3802133d4d5be7fe491
oscarmg <oscarmg.university@gmail.com>
2019-02-05 09:31:00 +0300

    Revert "Ft edit party #163682298"

--- app/api/app.py
+++
@@ -1,1 +0,0 @@
-## create app using config file
--- app/api/v1/models/Party.py
+++
@@ -1,6 +0,0 @@
-class PartyClass():
-    def __init__(self,id,name,hqAddress,logoUrl):
-        self.id=id
-        self.name=name
-        self.hqAddress=hqAddress
-        self.logoUrl=logoUrl
