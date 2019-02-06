ba98e854ecb8236405043c65f9efa60e8d113813
Muigai <oscarmg.university@gmail.com>
2019-02-06 07:49:56 +0300

    [start #163682200] creating office endpoint

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from app.api.blueprint import party
+from app.api.blueprint import office
 app=Flask(__name__)
 app.register_blueprint(party)  #register party blueprint
+app.register_blueprint(office)
 
 if __name__ == '__main__':
     app.run(debug =True)
