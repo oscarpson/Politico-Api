8ea2a841f5f56b1977206719c080513c769b6ef9
Muigai <oscarmg.university@gmail.com>
2019-02-07 21:35:36 +0300

    fix readme badges

--- app/api/v1/views/partyv.py
+++ app/api/v1/views/partyv.py
@@ -1,38 +1,29 @@
 from flask import Flask,request, jsonify, make_response,json
 from app.api.v1.models.Party import PartyClass as party
-import random
-#list of parties 
 parties_list=[]
-
 class parties():
     def get(self,id=None):
         if id !=None:
             partydata=[party for party in parties_list if party["id"] == int(id)]
             if len(partydata) < 1:
-                return jsonify({'status':404,'error':'party does not exist'})
-            return jsonify({'status':200,'data':{'id':partydata[0]["id"],'name':partydata[0]["name"],'logoUrl':partydata[0]["logoUrl"]}})    
-        
-        return make_response(jsonify({"status":200},{"data":parties_list}),200)
-        
+                return make_response(jsonify({'status':404,'error':'party does not exist'}),404)
+            return jsonify({'status':200,'data':{'id':partydata[0]["id"],'name':partydata[0]["name"],'logoUrl':partydata[0]["logoUrl"]}})            
+        return make_response(jsonify({"status":200},{"data":parties_list}),200)     
 
-    def post(self):       
+    def post(self): 
+        
         partyjson=request.get_json(force= True)        
-        name=partyjson['name']    ## ERROR!
+        name=partyjson['name']    
         hqAddress=partyjson['hqAddress']
         logoUrl=partyjson['logoUrl']        
         id=len(parties_list)+1
         myparty={"id":id,"name":name,"hqAddress":hqAddress,"logoUrl":logoUrl}#create new party object       
         parties_list.append(myparty)                
-        return jsonify({'status':'201'},{'data':[myparty]})
-
-
-    
+        return jsonify({'status':'201'},{'data':[myparty]})    
     def delete(self,id):
-
         if not 'id' :
             return jsonify({'status':400,'error':'party id cannot be null'} )        
-        #partyjson=request.get_json()
-        #self.id=id 
+    
         party_to_delete=[party for party in parties_list if party["id"] == int(id)] 
         if len(party_to_delete) < 1 :
             return jsonify({'status':400,'error':'party does not exist'})  
