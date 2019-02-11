961549f3d1afa1ba4e5f69311d8faa1b93476b43
Muigai <oscarmg.university@gmail.com>
2019-02-11 20:25:18 +0300

    fix feedback changes

---
+++ app/instance/config.py
@@ -0,0 +1,37 @@
+import os
+
+
+class Config(object):
+    """Parent configuration class."""
+    DEBUG = False    
+    SECRET = os.getenv('SECRET')
+    
+
+
+class DevelopmentConfig(Config):
+    """Configurations for Development."""
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    """Configurations for Testing"""
+    TESTING = True    
+    DEBUG = True
+
+class StagingConfig(Config):
+    """Configurations for Staging."""
+    DEBUG = True
+
+
+class ProductionConfig(Config):
+    """Configurations for Production."""
+    DEBUG = False
+    TESTING = False
+
+
+app_config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'staging': StagingConfig,
+    'production': ProductionConfig,
+}
